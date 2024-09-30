89bdbdf1b13e7aa14ae9febe93b33f5359b80334
Leartdemiri <114229331+Leartdemiri@users.noreply.github.com>
2024-09-30 13:05:13 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const DATA_KEY = "Data";
+const DATA_LINK = "https://restcountries.com/v3.1/all";
+let countryData;
+
+async function fetchCountryData() {
+    if (localStorage.getItem(DATA_KEY) !== null) {
+        countryData = JSON.parse(localStorage.getItem(DATA_KEY));
+    } else {
+        const response = await fetch(DATA_LINK);
+        countryData = await response.json();
+        localStorage.setItem(DATA_KEY, JSON.stringify(countryData));
+    }
+    displayCountryList(countryData);
+}
+
+function displayCountryList(countries) {
+    const countryListElement = document.getElementById("country-list");
+    countryListElement.innerHTML = '';
+
+    countries.forEach(country => {
+        const listItem = document.createElement("li");
+
+        // Create and append the flag image
+        const flagImg = document.createElement("img");
+        flagImg.src = country.flags.svg;
+        flagImg.alt = `Drapeau de ${country.name.common}`;
+        flagImg.style.width = "50px";
+        flagImg.style.marginRight = "10px";
+
+        listItem.appendChild(flagImg);
+
+        listItem.appendChild(document.createTextNode(country.name.common));
+
+        countryListElement.appendChild(listItem);
+    });
+}
+
+fetchCountryData();
