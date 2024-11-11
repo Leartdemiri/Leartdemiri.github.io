cc802712499cd461cb9b37324e3c77700e92c26d
Leartdemiri <leart.dmr2@eduge.ch>
2024-11-11 13:09:25 +0100

    GeoCaching

--- app.js
+++ app.js
@@ -1,47 +1,21 @@
-document.getElementById('fetch-contacts').addEventListener('click', async () => {
-  if ('contacts' in navigator && 'ContactsManager' in window) {
-    const props = ["name", "email", "tel"];
-    const opts = { multiple: true };
+/*"use strict"*/
 
-    try {
-      const contacts = await navigator.contacts.select(props, opts);
-      displayContacts(contacts);
-    } catch (err) {
-      console.error("Erreur lors de l'accès aux contacts : ", err);
-    }
-  } else {
-    alert("L'API ContactsManager n'est pas supportée sur cet appareil.");
-  }
-});
+import { Geolocation } from '@capacitor/geolocation';
 
-function displayContacts(contacts) {
-  const contactList = document.getElementById('contact-list');
-  contactList.innerHTML = ''; // Vider la liste avant de l'afficher
+let div = document.getElementById("my-position");
 
-  contacts.forEach(contact => {
-    const contactCard = document.createElement('div');
-    contactCard.className = 'contact-card';
+const printCurrentPosition = async () => {
+  const coordinates = await Geolocation.getCurrentPosition();
 
-    // Nom
-    const name = contact.name ? contact.name[0] : 'Nom inconnu';
-    const nameElement = document.createElement('h2');
-    nameElement.textContent = name;
-    contactCard.appendChild(nameElement);
+  let str = "";
+  str += 'Latitude:'+ coordinates.coords.latitude + '<br>';
+  str += 'Longitude:'+ coordinates.coords.longitude + '<br>';
+  div.innerHTML = str;
 
-    // Email
-    if (contact.email) {
-      const emailElement = document.createElement('p');
-      emailElement.textContent = `Email: ${contact.email[0]}`;
-      contactCard.appendChild(emailElement);
-    }
+  alert("SA MARCHE");
 
-    // Tel
-    if (contact.tel) {
-      const telElement = document.createElement('p');
-      telElement.textContent = `Téléphone: ${contact.tel[0]}`;
-      contactCard.appendChild(telElement);
-    }
+};
 
-    contactList.appendChild(contactCard);
-  });
-}
+document.getElementById("my-position-button").addEventListener("click", printCurrentPosition);
+
+// printCurrentPosition();
